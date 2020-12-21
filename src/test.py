0b7d8373e3891ea8fe6acb3f5370e8b1fa9c78b9
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-12-21 23:05:52 +0900

    change test value

--- src/test.py
+++ src/test.py
@@ -2,9 +2,10 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
-def sub(a: int, b: int) -> int:
+def sub(a: int, b: int) -> None:
     return a - b
 
 
-def div(a: int, b: int) -> None:
-    return a // b
+def div(a: int, b: int) -> int:
+    if b != 0:
+        return a // b
