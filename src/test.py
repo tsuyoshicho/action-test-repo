9a407f5c6c0376905e932df24e2fde1c9499658b
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2021-05-15 21:19:05 +0900

    Revert "add error"
    This reverts commit 8f22923bc01fcfeab95bcd23f52225b566aa7f40.

--- src/test.py
+++ src/test.py
@@ -2,10 +2,12 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
-def sub(a: int, b: int) -> None:
+def sub(a: int, b: int) -> int:
     return a - b
 
 
 def div(a: int, b: int) -> int:
     if b != 0:
         return a // b
+    else:
+        return 0
